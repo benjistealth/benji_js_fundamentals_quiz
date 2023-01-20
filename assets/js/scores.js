0c5384c2c9b9e93d889f18c299efae3c4d8552ad
Ben <stealthton@gmail.com>
2023-01-20 15:44:36 +0000

    object recalling score, clear can be spammed

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,7 +5,8 @@ var highscoresList = document.querySelector("#highscores");
 var clearScores = document.querySelector("#clear");
 var recalledScores;
 var recalledInitials;
-var recalledArr = [""];
+var recalledArr = [];
+var recalledObj;
 
 
 // event listener for submit button so save the data
@@ -15,12 +16,14 @@ if (submitButton) {
         event.preventDefault();
         var saveInitials = intialsEl.value;
         var saveScore = timerCount;
+        
         var finalScore = {
             initials: saveInitials,
             score: saveScore,
         };
-        localStorage.setItem("initials", saveInitials);
-        localStorage.setItem("score", saveScore);
+        localStorage.setItem("finalScore", JSON.stringify(finalScore));
+        // localStorage.setItem("initials", saveInitials);
+        // localStorage.setItem("score", saveScore);
         changePage();
     });
 }
@@ -32,35 +35,46 @@ function changePage() {
 
 function getScores() {
     // recall scores from local storage
-    //var recalledString = JSON.parse(localStorage.getItem("finalscore")); // not working
-    recalledInitials = localStorage.getItem("initials");
-    recalledScores = localStorage.getItem("score");
-    recalledArr[0] = [recalledInitials + " - " + recalledScores];
+    recalledObj = JSON.parse(localStorage.getItem("finalScore"));
+    if (recalledObj !== null) {
+        recalledInitials = recalledObj.initials;
+        recalledScores = recalledObj.score;
+        // recalledInitials = localStorage.getItem("initials");
+        // recalledScores = localStorage.getItem("score");
+        console.log(recalledInitials + " - " + recalledScores);
+        recalledArr.push(recalledInitials + " - " + recalledScores);
+    }
 }
 
 displayScores()
 // displaying the scores does not seem to work and I cannot see why
-//  get the latest saved scores & display on highscores pagerecalledScores
-    // attempt to populate new element created
+//  get the latest saved scores & display on highscores
+// attempt to populate new element created
 
 function displayScores() {
-    if (highscoresList){    
-    getScores();
-    for (let i = 0; i < recalledArr.length; i++) {
-        let item = recalledArr[i];
-        let listItem = document.createElement("li");
-        listItem.textContent = item;
-        highscoresList.appendChild(listItem);
+    if (highscoresList) {
+        getScores();
+        for (let i = 0; i < recalledArr.length; i++) {
+            let item = recalledArr[i];
+            console.log(item);
+            let listItem = document.createElement("li");
+            listItem.textContent = item;
+            highscoresList.appendChild(listItem);
+        }
+
     }
- }
 }
 
 // add listener for clear scores button
-if (clearScores) {
+if (window.location.href.endsWith("/highscores.html")){
+
     clearScores.addEventListener("click", function (event) {
+        let listItem = document.querySelector("li");
         event.preventDefault();
-        localStorage.clear();
-        var listElement = document.getElementById('highscores');
-        listElement.parentNode.removeChild(listElement);
+        if (listItem) {
+            localStorage.clear();
+            var listElement = document.getElementById('highscores');
+            listElement.parentNode.removeChild(listElement);
+        }
     });
 }
